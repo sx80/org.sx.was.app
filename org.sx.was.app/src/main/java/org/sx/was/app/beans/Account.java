68295f2de18ccb7b342aec7a2d8fcd6bb3f7fea2
joe black <joe black@MININT>
2016-09-21 17:39:47 +0200

    Initial 01

---
+++ org.sx.was.app/src/main/java/org/sx/was/app/beans/Account.java
@@ -0,0 +1,39 @@
+package org.sx.was.app.beans;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class Account implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private String value = "to jest text";
+	
+	public Account() {
+		
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	@PostConstruct
+	public void postConstruct(){
+		System.out.println("--------------------------------postConstruct" + this);
+	}
+	
+	@PreDestroy
+	public void preDestroy(){
+		System.out.println("--------------------------------preDestroy" + this);
+	}
+}
